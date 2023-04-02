c5251e34f99f9735d2889f9f9ce0ac7f1a30a412
lethinhuy-b1910181 <yb1910181@student.ctu.edu.vn>
2023-04-02 22:17:20 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,20 @@ const routes = [
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
         },
-        
+
+    {
+        path: "/add",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+        },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+        },
+            
+    
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
